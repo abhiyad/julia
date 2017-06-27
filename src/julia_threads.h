215fb8384af75086b29c6995ab8b67d9b8757020
kpamnany <kiran.pamnany@intel.com>
2017-06-27 17:53:49 +0530

    Integrating partr in progress
    Added partr code. Abstracted interface to threading infrastructure.
    Almost complete. Julia side in progress (task.jl, event.jl,
    channels.jl, etc.?). Successfully builds basecompiler, but fails
    on building the system image.

--- src/julia_threads.h
+++ src/julia_threads.h
@@ -159,6 +159,10 @@ struct _jl_tls_states_t {
 //#endif
     jl_jmp_buf *safe_restore;
     int16_t tid;
+#ifdef JULIA_ENABLE_PARTR
+    uint64_t rngseed;
+    struct _jl_taskq_t *sticky_taskq;
+#endif
     size_t bt_size;
     // JL_MAX_BT_SIZE + 1 elements long
     uintptr_t *bt_data;
